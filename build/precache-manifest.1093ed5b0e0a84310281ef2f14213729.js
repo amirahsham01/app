4067ecdfa0441deee5655183048e910e6e307ff6
Nur Amirah Sham <nuramirahsham@gmail.com>
2020-09-03 17:21:11 +0800

    deploying app

---
+++ build/precache-manifest.1093ed5b0e0a84310281ef2f14213729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35df05537ea96065baf5a58aa5e3c041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3c545959d0c2bbc992",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3392fc58e99aad680cf1",
+    "url": "/static/css/main.0f0acb33.chunk.css"
+  },
+  {
+    "revision": "7e3c545959d0c2bbc992",
+    "url": "/static/js/2.75268756.chunk.js"
+  },
+  {
+    "revision": "ce84f45c6e623bd7d8c4f820fd2feb3f",
+    "url": "/static/js/2.75268756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3392fc58e99aad680cf1",
+    "url": "/static/js/main.7d3c4fe3.chunk.js"
+  },
+  {
+    "revision": "5e4407fa3c504d226775",
+    "url": "/static/js/runtime-main.1e5016ed.js"
+  }
+]);
